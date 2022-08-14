53a74e2dbe9e6b71a2b31acce6260fc2793560dd
sungchol3 <64838162+sungchol3@users.noreply.github.com>
2022-08-15 00:11:05 +0900

    Add getText() function
    Add function which are reading local txt file and whose name is getText

--- docs/main.js
+++ docs/main.js
@@ -1,3 +1,22 @@
 var result = document.querySelector("#result");
 var reader = new FileReader();
-reader.onload = () => {};
+let text = "";
+reader.onload = () => {
+    text = getText('Bus_weekday_cw.csv');
+    result.innerHTML = text;
+};
+
+function getText(url){
+    // read text from URL location
+    var request = new XMLHttpRequest();
+    request.open('GET', url, true);
+    request.send(null);
+    request.onreadystatechange = function () {
+        if (request.readyState === 4 && request.status === 200) {
+            var type = request.getResponseHeader('Content-Type');
+            if (type.indexOf("text") !== 1) {
+                return request.responseText;
+            }
+        }
+    }
+}
