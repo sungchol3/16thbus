5bfc1f880931afbcab04a88e9874252bce3621cf
sungchol3 <64838162+sungchol3@users.noreply.github.com>
2022-08-15 13:29:15 +0900

    Edit reading csv part using jquery

--- docs/main.js
+++ docs/main.js
@@ -6,19 +6,13 @@ function load(){
 
 function readTextFile(file)
 {
-    var rawFile = new XMLHttpRequest();
-    rawFile.open("GET", file, false);
-    rawFile.onreadystatechange = function ()
-    {
-        if(rawFile.readyState === 4)
-        {
-            if(rawFile.status === 200 || rawFile.status == 0)
-            {
-                var allText = rawFile.responseText;
-                alert(allText);
-                return allText;
-            }
-        }
-    }
-    rawFile.send(null);
+    $.ajax({
+        url: 'Bus_weekday_cw.csv',
+        dataType: 'text',
+    }).done(successFunction);
+}
+
+function successFunction(data) {
+    var array = $.csv.toArrays(data);
+    alert(array);
 }
